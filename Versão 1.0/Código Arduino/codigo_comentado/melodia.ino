4de7ea2d610bbeb2d686cd84c4936656cfd9c8fb
Lucas <byu031@hotmail.com>
2017-05-16 20:25:21 -0300

    Melodia documentada

--- Versão 1.0/Código Arduino/codigo_comentado/melodia.ino
+++ Versão 1.0/Código Arduino/codigo_comentado/melodia.ino
@@ -3,14 +3,14 @@
 
 void melodia(){
   //Indicação de entrada
-  flag = 0;
+  flag = 0; 
   int tempo = 0;
   char v[TAM], n[TAM];
-  for (int i = 0; i < TAM; i++) {
+  for (int i = 0; i < TAM; i++) { //Preenche os vetores n e v com o caracter x.
     n[i] = 'x'; 
     v[i] = 'x';
   }
-  while (flag == 0) {
+  while (flag == 0) { // Enquanto a variável flag for 0, lê o número de bytes até encontrar o caracter x, ou até o tempo acabar.
     while (!Serial.available());
     Serial.setTimeout(60000);
     Serial.readBytesUntil('x', v, TAM);
@@ -19,7 +19,7 @@ void melodia(){
       flag++;
     }
 
-    for (int j = 0; v[j] != 'x' && flag == 0; j++) {
+    for (int j = 0; v[j] != 'x' && flag == 0; j++) { // Laço para definir o tom para tocar uma nota,se o caracter do vetor v for 1.
       int w = 0;
       if (v[j] == 'm') {
         j++;
@@ -30,18 +30,18 @@ void melodia(){
             delay2(pausadepoisdasnotas[nota]);
           }
         }
-        if (v[j] == '2') {
+        if (v[j] == '2') { // Define um tom específico caso o caracter na posição j do vetor v seja 2.
           tone(9, 600, 1000); 
           delay2(1000);
         }
       }
-      if (v[j] >= '0' && v[j] <= '9') {
+      if (v[j] >= '0' && v[j] <= '9') { // Caso o caracter na posição j do vetor estiver entre 0 e 9, copia esse valor para o vetor n.
         for (w = 0; v[j] >= '0' && v[j] <= '9'; j++ ) {
           n[w] = v[j]; 
           w++;
         }
         j = j - 1;
-        tempo = atoi(n);
+        tempo = atoi(n);// Converte os caracteres do vetor n em números.
         if (v[j - w] == 'f') {
           motor1.attach(SERVO1);
           motor2.attach(SERVO4);
@@ -83,12 +83,12 @@ void melodia(){
           delay2(DMOTOR);
         }
         tempo = 0;
-        for (int i = 0; i < TAM; i++) {
+        for (int i = 0; i < TAM; i++) { // Preenche todas as posições do vetor n com o caracter p.
           n[i] = 'p';
         }
       }
     }
-    for (int i = 0; i < TAM; i++) {
+    for (int i = 0; i < TAM; i++) {// Preenche todas as posições do vetor n e v com o caracter x.
       n[i] = 'x'; 
       v[i] = 'x';
     }
