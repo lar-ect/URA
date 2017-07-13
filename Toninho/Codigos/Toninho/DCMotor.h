62501a1b3ff684a6579f7d3f66ef5c66212e000e
samuel-cavalcanti <scavalcanti111@gmail.com>
2017-07-13 00:58:55 -0300

    criado o branch FCT (fã clube do Toninho )

---
+++ Toninho/Codigos/Toninho/DCMotor.h
@@ -0,0 +1,34 @@
+#ifndef DCMOTOR_H
+#define DCMOTOR_H
+
+
+
+
+class DCMotor 
+{
+  public:
+    DCMotor(int in1, int in2, int in3, int in4);
+    void forward();
+    void forward2();
+    void backward();
+    void left();
+    void right(); 
+    void stop(); 
+    void leftBack();
+    void rightBack();
+    bool slowMode();
+
+  private:
+    void clockwise(int inx, int iny);
+    void antiClockwise(int inx, int iny); 
+    int _in1, _in2, _in3, _in4; 
+    unsigned long currentMillis;
+    unsigned long previousMillis = millis();
+    unsigned long interval = 2000;
+    bool estado;
+    //const int  IN1 = 5, IN2 = 4, IN3 = 3, IN4 = 2;//pinos
+  
+};
+
+#endif  //endif DCMotor
+
