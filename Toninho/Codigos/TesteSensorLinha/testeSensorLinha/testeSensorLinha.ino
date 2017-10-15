c5058eed14c69643058832ecd35f0a27cb1ac3fd
samuel-cavalcanti <scavalcanti111@gmail.com>
2017-10-15 19:38:34 -0300

    att na Wiki

--- Toninho/Codigos/TesteSensorLinha/testeSensorLinha/testeSensorLinha.ino
+++ Toninho/Codigos/TesteSensorLinha/testeSensorLinha/testeSensorLinha.ino
@@ -12,7 +12,7 @@ int pinoAnalogico = 1;
    pinoAnalogico, que  possui o valor igual a 1
 */
 
-SensorLinha meuSensorLinha(1);
+SensorLinha meuSensorLinha(pinoAnalogico);
 
 /* escrevendo: "SensorLinha meuSensorLinha" .  você está declarando
     um tipo de variavel especial chamando ela de meuSensorLinha.
@@ -42,13 +42,34 @@ void setup() {
 void loop() {
 
   Serial.print("Sensor: ");
-  Serial.println( analogRead(_sensor) );
+  Serial.println( analogRead(pinoAnalogico) );
+
 
   /*
-     analogRead(_sensor),  essa função exibe valores entre 0 a 1023, que remetem a
+     analogRead(pinoAnalogico),  essa função exibe valores entre 0 a 1023, que remetem a
      intencidade da luz , onde quando maior o valor mais escura é a cor capturada.
      já a função Serial.print(), perimite printar ou exibir esse valores no monitor Serial
+     utilizei essas duas funções para encontrar o meu limiar
   */
+
+
+
+  if ( meuSensorLinha.detectedBySensor() == true ){
+      Serial.print("preto detectado");
+
+   }
+  else{ // significa que meuSensorLinha.detectedBySensor() == false 
+      Serial.print("preto não detectado");
+   }
+   /*
+      .detectedBySensor() é uma função especial do tipo SensorLinha, ela retorna apenas
+      dois valores, verdadeiro ou falso, caso o valor detectado pelo sensor linha for
+      maior que  seu limiar definido anteriormente, essa função retorna verdadeiro ou seja
+      a cor do objeto que está abaixo do sensor é tão escura que podemos concluir que é o preto
+      se não retornar verdadeiro, ou seja se for falso então a cor muito clara para podermos dizer
+      que é preto. 
+
+   */
 
 
 
